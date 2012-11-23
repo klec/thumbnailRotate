9d950777b02b2c4ffd5a32320745c687ed7fa0ab
Yuri <klechome@mail.ru>
2012-11-23 23:15:28 +0200

    Setup for module
    Add Rotate attribute and Tab

---
+++ Model/Observer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: klec
+ * Date: 22.11.12
+ * Time: 21:22
+ * To change this template use File | Settings | File Templates.
+ */
+class Klec_Rotate_Model_Observer extends Mage_Core_Model_Abstract
+{
+
+    /**
+     * set default method
+     *
+     * @var $quote Idev_OneStepCheckout_Model_Sales_Quote
+     * @param Varien_Event_Observer $observer
+     */
+    public function addTab(Varien_Event_Observer $observer)
+    {
+        //echo $observer;
+    }
+
+}
---
+++ Model/Setup.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: klec
+ * Date: 25.01.12
+ * Time: 19:23
+ * To change this template use File | Settings | File Templates.
+ */
+class Klec_Rotate_Model_Setup extends Mage_Eav_Model_Entity_Setup
+{
+
+    public function addRotateTab()
+    {
+        $imageTab=  $this->getAttributeGroup('4', '4', 'Images');
+        $this->addAttributeGroup('4', '4', 'Rotate Images',$imageTab['sort_order']);
+    }
+
+    function addMediaGallery()
+    {
+        $attr = array(
+            'backend' => 'catalog/product_attribute_backend_media',
+            'type' => 'varchar',
+            'input' => 'gallery',
+            'label' => 'Rotate gallery',
+            'source' => 'eav/entity_attribute_source_boolean',
+            'required' => 0,
+            'user_defined' => 0
+        );
+        $this->addAttribute('4', 'rotate_gallery', $attr);
+        if ($groupId = $this->getAttributeGroupId('4', '4', 'Rotate Images')) {
+            $this->addAttributeToGroup('4', '4', $groupId, $this->getAttributeId('4', 'rotate_gallery'));
+        }
+    }
+}
