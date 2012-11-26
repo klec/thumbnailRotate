740deb88270c259d4e94b3a55acf6ba83fe4603e
Yuri <klechome@mail.ru>
2012-11-26 03:23:15 +0200

    frontend changes

--- sql/rotate_setup/mysql4-install-0.1.0.php
+++ sql/rotate_setup/mysql4-install-0.1.0.php
@@ -1,10 +1,9 @@
 <?php
 /**
  * Created by JetBrains PhpStorm.
- * User: klec
- * Date: 25.01.12
- * Time: 19:27
  * To change this template use File | Settings | File Templates.
+ * @author klec
+ * @var $this Mage_Eav_Model_Entity_Setup
  */
 $this->addRotateTab();
 $this->addMediaGallery();
